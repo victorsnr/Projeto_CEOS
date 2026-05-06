a8780925ef6178a97c9315c8478f203026565ff0
victorsnr <irsamuelrodriguesv@gmail.com>
2026-05-05 22:10:47 -0300

    Script da barra de busca

---
+++ static/js/busca_apostadores.js
@@ -0,0 +1,22 @@
+const procurar = document.getElementById('procurarApostador');
+
+procurar.addEventListener('input', () => {
+    const termo = procurar.value.toLowerCase();
+
+    const apostadores = document.querySelectorAll('.card-apostador');
+
+    apostadores.forEach(apostador => {
+        const id = apostador.dataset.id;
+        const nome = apostador.dataset.nome.toLowerCase();
+        const cpf = apostador.dataset.cpf;
+        const email = apostador.dataset.email.toLowerCase();
+
+        const corresponde = 
+            id.includes(termo) ||
+            nome.includes(termo) ||
+            cpf.includes(termo) ||
+            email.includes(termo);
+
+        apostador.style.display = corresponde ? 'block' : 'none';
+    });
+});
