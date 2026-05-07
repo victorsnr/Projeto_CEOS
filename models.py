5120dc2ed07cd6bb1251ef926cf7863060b0ef8d
victorsnr <irsamuelrodriguesv@gmail.com>
2026-05-07 17:21:58 -0300

    Otimização de memória

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ class Apostador(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    telefone = db.Column(db.String(20), nullable=False)
+    telefone = db.Column(db.String(11), nullable=False)
     cpf = db.Column(db.String(11), unique=True, nullable=False)
     dt_nasc = db.Column(db.Date, nullable=False)
 
